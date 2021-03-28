5dc727170e97ec347241c4d41b496a046f30a1a1
Rose Martus <martus.rose@gmail.com>
2021-03-28 17:39:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+/*changes every element in `array` to `changeValue` until it reaches `stopValue`*/
+function breakOut(array, changeValue, stopValue){
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === stopValue){
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+/*changes every element in `array` to `changeValue` unless the element === `skipValue`*/
+function keepGoing(array, changeValue, skipValue){
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === skipValue){
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
+/*finds the a value that satisfies FindFn and returns null if value is not found*/
+function findBy(array, findFn){
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === findFn){
+      return array[i]
+    }
+  }
+}
