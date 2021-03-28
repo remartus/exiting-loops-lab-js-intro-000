2cd6726be4acccfd93b88167d7a2ba4fbf9a908b
Rose Martus <martus.rose@gmail.com>
2021-03-28 17:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function keepGoing(array, changeValue, skipValue){
 function findBy(array, findFn){
   for(let i = 0; i < array.length; i++){
     if(array[i] === findFn){
-      return `findFn`
+      return findFn
     }
   }
   return null
