2fecf3bdb6c9205b24178d3929ce73763a6890a7
Rose Martus <martus.rose@gmail.com>
2021-03-28 17:47:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function keepGoing(array, changeValue, skipValue){
 function findBy(array, findFn){
   const c = findFn
   for(let i = 0; i < array.length; i++){
-    if(array[i] === case){
+    if(array[i] === c){
       return c
     }
   }
